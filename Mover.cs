7484ad1ac9f495a470ed76b5f275f41d267979b4
Nicole Polidore <42782119+nicolepolidore@users.noreply.github.com>
2018-09-25 00:49:39 -0400

    Add files via upload

---
+++ Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
